12a986f9b1a86433b2e6c95ac1759bb3112f73e8
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-05-13 16:13:21 +0100

    Lecture 47 - Done.
    It was just bug fixes and adding in angular drag and what not.

--- Project Boost project/Project Boost/Assets/Rocket.cs
+++ Project Boost project/Project Boost/Assets/Rocket.cs
@@ -17,31 +17,41 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
-	}
+        Rotate();
+        Thrust();
+    }
 
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up); // up uses the y axis.
             if (!audioSource.isPlaying) // So that it doesn't keep playing the same clip on top of eachother.
             {
-            audioSource.Play();
+                audioSource.Play();
             }
         } else
         {
             audioSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation (ignores physics collisions)
+
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward); // forward is a misleading naming. It is not forward, it is a positive z axis rotating the object around that axis by a number/value of 1.
                                                // Think of the z axis turning right or left as a gear and connected to it by a pole is the object moving in a x axis.
                                                // I hate the way that Unity uses axis and how it names things like forward.
                                                // You want to rotate something in the z axis? Well don't use the z axis for that!
-        } else if (Input.GetKey(KeyCode.D))
+        }
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward); // You can also use .back
         }
+
+        rigidBody.freezeRotation = false; // resume physics engine control of rotation
     }
 }
