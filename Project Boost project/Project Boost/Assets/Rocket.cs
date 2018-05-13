9e6e5f8f6c6a099fd0467ca9d819146eee7d0006
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-05-13 14:45:09 +0100

    Lecture 46 - Done.
    An audio source for the thruster has been added.

--- Project Boost project/Project Boost/Assets/Rocket.cs
+++ Project Boost project/Project Boost/Assets/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 
     }
 	
@@ -23,13 +25,20 @@ public class Rocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up); // up uses the y axis.
+            if (!audioSource.isPlaying) // So that it doesn't keep playing the same clip on top of eachother.
+            {
+            audioSource.Play();
+            }
+        } else
+        {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward); // forward uses the z axis. By pressing A the ship goes anti clockwise/minus x. It looks like minus x to me. This is angering me.
-                                               // I guess that forward and backward are not doing what Ben is saying. He probably hasn't noticed that x is the left and or right not z.
-                                               // The Unity docs state that z is the axis of forward but our game doesn't follow that.
-                                               // We won't use the z axis in this 2.5D game. He hasn't mentinoed that it is 2.5D but right now it seems to be that.
+            transform.Rotate(Vector3.forward); // forward is a misleading naming. It is not forward, it is a positive z axis rotating the object around that axis by a number/value of 1.
+                                               // Think of the z axis turning right or left as a gear and connected to it by a pole is the object moving in a x axis.
+                                               // I hate the way that Unity uses axis and how it names things like forward.
+                                               // You want to rotate something in the z axis? Well don't use the z axis for that!
         } else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward); // You can also use .back
