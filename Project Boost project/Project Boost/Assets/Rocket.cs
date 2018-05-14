5064a829acfb6845ee4800de931030b7db544c1c
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-05-14 13:09:28 +0100

    Lecture 49 - Done.
    It covered serialise field and the differences between using it and using public.
    Also the trust for up and rotation can be adjusted in the inspector.

--- Project Boost project/Project Boost/Assets/Rocket.cs
+++ Project Boost project/Project Boost/Assets/Rocket.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
+
     Rigidbody rigidBody;
     AudioSource audioSource;
 
@@ -25,7 +29,7 @@ public class Rocket : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up); // up uses the y axis.
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust); // up uses the y axis.
             if (!audioSource.isPlaying) // So that it doesn't keep playing the same clip on top of eachother.
             {
                 audioSource.Play();
@@ -40,16 +44,19 @@ public class Rocket : MonoBehaviour {
     {
         rigidBody.freezeRotation = true; // take manual control of rotation (ignores physics collisions)
 
+        float rotationThisFrame = rcsThrust * Time.deltaTime; // rotationThisFrame based on the thrust.
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward); // forward is a misleading naming. It is not forward, it is a positive z axis rotating the object around that axis by a number/value of 1.
+            transform.Rotate(Vector3.forward * rotationThisFrame); // forward is a misleading naming. It is not forward, it is a positive z axis rotating the object around that axis by a number/value of 1.
                                                // Think of the z axis turning right or left as a gear and connected to it by a pole is the object moving in a x axis.
                                                // I hate the way that Unity uses axis and how it names things like forward.
                                                // You want to rotate something in the z axis? Well don't use the z axis for that!
+                                               // CAN ALSO use transform.Rotate(new Vector3(0f, 0f, 1f)); instead of of .forward
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward); // You can also use .back
+            transform.Rotate(-Vector3.forward * rotationThisFrame); // You can also use .back
         }
 
         rigidBody.freezeRotation = false; // resume physics engine control of rotation
